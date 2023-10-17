e329025e9c121ae3f0315cb5ca42eb9ef71561fd
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-10-17 14:44:17 +0200

    git init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          Week number. (DOT) Short course code. (DOT) Your first name. (DOT) The
+          first letter of your surname. For example, to submit a course
+          assignment for w04 (design foundations), the files submitted by John
+          Doe and Sarah Iversen must be named like this:
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
