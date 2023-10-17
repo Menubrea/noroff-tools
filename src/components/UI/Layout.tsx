8b626c2c442f91f908300587361aa77bbf3b0f0b
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-10-17 18:51:00 +0200

    Barrel export files

---
+++ src/components/UI/Layout.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Box, Typography, Link } from "@mui/material";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Box
+        component={"header"}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#fafafa",
+          padding: 2,
+          top: 0,
+          left: 0,
+          position: "fixed",
+          width: "calc(100% - 2rem)",
+          boxShadow: "0 0 10px 1px #00000019",
+        }}
+      >
+        <Typography
+          fontSize={20}
+          fontWeight={900}
+          component="div"
+          sx={{ display: "flex", gap: 1.5, alignItems: "center" }}
+        >
+          <ConstructionIcon
+            color="primary"
+            sx={{
+              padding: 0.5,
+              borderRadius: 1,
+              boxShadow: "0 0 10px 1px #00000018",
+            }}
+          />
+          Student Toolbox
+        </Typography>
+      </Box>
+      <Box component={"main"}>{children}</Box>
+      <Box
+        sx={{
+          backgroundColor: "#fafafa",
+          padding: 2,
+          bottom: 0,
+          left: 0,
+          position: "fixed",
+          width: "calc(100% - 2rem)",
+          display: "flex",
+          justifyContent: "center",
+        }}
+        component={"footer"}
+      >
+        <Box marginRight={1}>By Truls Haakenstad &copy; 2023 |</Box>
+        <Link
+          sx={{
+            display: "flex",
+            gap: 0.5,
+            alignItems: "center",
+            fontWeight: 700,
+            textDecoration: "none",
+          }}
+          href="https://github.com/Menubrea"
+          target="_blank"
+          rel="noopener"
+        >
+          <GitHubIcon sx={{ fontSize: 20 }} />
+          Github
+        </Link>
+      </Box>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export * from "./forms";
+// Path: src/components/forms/index.ts
+
+export * from "./UI";
+// Path: src/components/index.ts
+
+export * from "./links";
+// Path: src/components/links/index.ts
